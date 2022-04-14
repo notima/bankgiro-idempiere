a24fff427378d080be748ac9c5f4382082a55cbd
Daniel Tamm <daniel@notima.se>
2022-04-14 17:31:07 +0200

    Add crossborder payment capability.

--- iso20022/src/main/java/org/notima/idempiere/iso20022/Iso20022FileFactory.java
+++ iso20022/src/main/java/org/notima/idempiere/iso20022/Iso20022FileFactory.java
@@ -18,8 +18,10 @@ import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.compiere.model.MBPBankAccount;
 import org.compiere.model.MBPartner;
+import org.compiere.model.MBPartnerLocation;
 import org.compiere.model.MBankAccount;
 import org.compiere.model.MInvoice;
+import org.compiere.model.MLocation;
 import org.compiere.model.MOrg;
 import org.compiere.util.Env;
 import org.notima.bankgiro.adempiere.BankAccountUtil;
@@ -30,8 +32,6 @@ import org.notima.bankgiro.adempiere.PaymentValidator;
 import org.notima.bankgiro.adempiere.PluginRegistry;
 import org.notima.bankgiro.adempiere.model.MLBSettings;
 import org.notima.bg.BgUtil;
-import org.notima.idempiere.iso20022.entity.pain.ClearingSystemIdentification2Choice;
-import org.notima.idempiere.iso20022.entity.pain.ClearingSystemMemberIdentification2;
 import org.notima.idempiere.iso20022.entity.pain.AccountIdentification4Choice;
 import org.notima.idempiere.iso20022.entity.pain.AccountSchemeName1Choice;
 import org.notima.idempiere.iso20022.entity.pain.ActiveOrHistoricCurrencyAndAmount;
@@ -39,6 +39,7 @@ import org.notima.idempiere.iso20022.entity.pain.AmountType3Choice;
 import org.notima.idempiere.iso20022.entity.pain.BranchAndFinancialInstitutionIdentification4;
 import org.notima.idempiere.iso20022.entity.pain.CashAccount16;
 import org.notima.idempiere.iso20022.entity.pain.CategoryPurpose1Choice;
+import org.notima.idempiere.iso20022.entity.pain.ChargeBearerType1Code;
 import org.notima.idempiere.iso20022.entity.pain.CreditTransferTransactionInformation10;
 import org.notima.idempiere.iso20022.entity.pain.CreditorReferenceInformation2;
 import org.notima.idempiere.iso20022.entity.pain.CreditorReferenceType1Choice;
@@ -54,9 +55,12 @@ import org.notima.idempiere.iso20022.entity.pain.PaymentIdentification1;
 import org.notima.idempiere.iso20022.entity.pain.PaymentInstructionInformation3;
 import org.notima.idempiere.iso20022.entity.pain.PaymentMethod3Code;
 import org.notima.idempiere.iso20022.entity.pain.PaymentTypeInformation19;
+import org.notima.idempiere.iso20022.entity.pain.PostalAddress6;
 import org.notima.idempiere.iso20022.entity.pain.Priority2Code;
+import org.notima.idempiere.iso20022.entity.pain.RegulatoryReporting3;
 import org.notima.idempiere.iso20022.entity.pain.RemittanceInformation5;
 import org.notima.idempiere.iso20022.entity.pain.ServiceLevel8Choice;
+import org.notima.idempiere.iso20022.entity.pain.StructuredRegulatoryReporting3;
 import org.notima.idempiere.iso20022.entity.pain.StructuredRemittanceInformation7;
 
 /**
@@ -89,6 +93,7 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 	private String				currentMsgId;
 	private int					currentPaymentId;
 	private static DatatypeFactory 	dataTypeFactory;
+	private String				countryCodeSrcAccount;
 	
 	static {
 		try {
@@ -187,40 +192,32 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 		Document doc = new Document();
 		CustomerCreditTransferInitiationV03 init = new CustomerCreditTransferInitiationV03();
 		doc.setCstmrCdtTrfInitn(init);
-		GroupHeader32 gh = new GroupHeader32();
-		init.setGrpHdr(gh);
+		GroupHeader32 groupHeader = new GroupHeader32();
+		init.setGrpHdr(groupHeader);
 		// Message ID
 		// MsgId
 		try {
-			gh.setMsgId(getNextMessageId(trxName));
+			groupHeader.setMsgId(getNextMessageId(trxName));
 		} catch (Exception e) {
 			e.printStackTrace();
 			MessageCenter.error("Can't get message ID for next file. Please check system settings");
 			return null;
 		}
-
 		// Date of file
-		// CreDtTm
 		XMLGregorianCalendar xcal;
 		xcal = dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar)Calendar.getInstance());
-
-		// Add payment info
-		List<PaymentInstructionInformation3> paymentList = init.getPmtInf();
-		PaymentInstructionInformation3 pmt;
-		
-		gh.setCreDtTm(xcal);
+		groupHeader.setCreDtTm(xcal);
 		
 		// Number of transactions
-		// NbOfTxs
-		gh.setNbOfTxs(Integer.toString(payments.size()));
-		
+		groupHeader.setNbOfTxs(Integer.toString(payments.size()));
+
 		// Initiator name
 		// InitgPty / Nm
 		MOrg info = MOrg.get(ctx, ba.getAD_Org_ID());
 		PartyIdentification32 sender = new PartyIdentification32();
 		sender.setNm(info.getName());
-		gh.setInitgPty(sender);
-		
+		groupHeader.setInitgPty(sender);
+
 		// Forward agent
 		BranchAndFinancialInstitutionIdentification4 bafii = new BranchAndFinancialInstitutionIdentification4();
 		FinancialInstitutionIdentification7 fii = new FinancialInstitutionIdentification7();
@@ -232,9 +229,13 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 		fii.setBIC(bic.trim().toUpperCase());
 		bafii.setFinInstnId(fii);
 		
-		gh.setFwdgAgt(bafii);
+		groupHeader.setFwdgAgt(bafii);
+
+		// Add payment info
+		List<PaymentInstructionInformation3> paymentList = init.getPmtInf();
+		PaymentInstructionInformation3 pmt;
 		
-        String countryCodeSrcAccount = ba.getIBAN().substring(0, 2).toUpperCase();
+        countryCodeSrcAccount = ba.getIBAN().substring(0, 2).toUpperCase();
 		
 		currentPaymentId = 1;
 		
@@ -297,10 +298,12 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 		// Payment Tp Inf
 		PaymentTypeInformation19 pmtTpInf = new PaymentTypeInformation19();
 		pmtTpInf.setInstrPrty(Priority2Code.NORM); // Always Normal
+		
 		// Service level
-		ServiceLevel8Choice svcLvl = new ServiceLevel8Choice();
-		svcLvl.setCd("NURG"); // Non-urgent
-		pmtTpInf.setSvcLvl(svcLvl);
+		ServiceLevel8Choice serviceLevel = new ServiceLevel8Choice();
+		serviceLevel.setCd("NURG"); // Non-urgent
+		pmtTpInf.setSvcLvl(serviceLevel);
+		
 		// Category Purpose
 		CategoryPurpose1Choice ctgyPurp = new CategoryPurpose1Choice();
 		ctgyPurp.setCd("SUPP"); // Always supplier payment
@@ -348,8 +351,6 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 		amt.setInstdAmt(amtCur);
 		trx.setAmt(amt);
 
-		trx.setCdtr(convert(lpr.getBpartner()));
-
 		// Set recipient account
 		
 		MBPBankAccount dstBa = lpr.getDstAccount();
@@ -366,6 +367,36 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 		
 		BankAccountUtil bau = BankAccountUtil.buildFromMBPBankAccount(dstBa);		
 
+		MLocation loc = getRemitToLocation(lpr.getBpartner());
+		
+		boolean crossCountry = loc!=null && !countryCodeSrcAccount.equalsIgnoreCase(loc.getCountry().getCountryCode());
+		
+		String iban = bau.getIban();
+		if (iban!=null && !iban.startsWith(countryCodeSrcAccount)) {
+			crossCountry = true;
+		}
+		
+		// Check if IBAN and EUR
+		if (bau.getAccountType() == BankAccountUtil.BankAccountType.IBAN && "EUR".equalsIgnoreCase(lpr.currency)) {
+			serviceLevel.setCd("SEPA");
+		}
+
+		trx.setCdtr(convert(lpr.getBpartner(), crossCountry));
+		
+		if (crossCountry) {
+			// Share charges
+			trx.setChrgBr(ChargeBearerType1Code.SHAR);
+			
+			// Set regularly reporting to Imports/Exports of goods
+			StructuredRegulatoryReporting3 srr = new StructuredRegulatoryReporting3();
+			srr.setCd("101"); // Imports / Exports
+			RegulatoryReporting3 rr = new RegulatoryReporting3();
+			rr.getDtls().add(srr);
+
+			trx.getRgltryRptg().add(rr);
+		}
+		
+		
 		BranchAndFinancialInstitutionIdentification4 bafiit = Iso20022Helper.getCreditorAgent(bau);
 		
 		trx.setCdtrAgt(bafiit);
@@ -471,8 +502,14 @@ public class Iso20022FileFactory implements PaymentFileFactory {
         if (bau.getAccountType().equals(BankAccountUtil.BankAccountType.PLUSGIRO) ||
         		bau.getAccountType().equals(BankAccountUtil.BankAccountType.DOMESTIC_BANKACCT)
         		) {
-        	other.setId(BgUtil.toDigitsOnly(bau.getDstAcct()));
         	ascheme.setCd("BBAN");
+        	String destinationAccount = null;
+        	if (bau.getAccountType().equals(BankAccountUtil.BankAccountType.DOMESTIC_BANKACCT)) {
+        		destinationAccount = bau.getAccountNo();
+        	} else {
+        		destinationAccount = bau.getDstAcct();
+        	}
+        	other.setId(BgUtil.toDigitsOnly(destinationAccount));
         	other.setSchmeNm(ascheme);
         	result.setOthr(other);
         }
@@ -489,10 +526,51 @@ public class Iso20022FileFactory implements PaymentFileFactory {
 	 * @param bp
 	 * @return
 	 */
-	private PartyIdentification32 convert(MBPartner bp) {
+	private PartyIdentification32 convert(MBPartner bp, boolean includeAddress) {
 		PartyIdentification32 result = new PartyIdentification32();
 		result.setNm(bp.getName());
+		
+		if (includeAddress) {
+			MLocation loc = getRemitToLocation(bp);
+			if (loc!=null) {
+				result.setCtryOfRes(loc.getCountry().getCountryCode());
+				result.setPstlAdr(convertLocation(loc));
+			}
+		}
+		
 		return result;
+	}
+
+	private PostalAddress6 convertLocation(MLocation loc) {
+		PostalAddress6 dst = new PostalAddress6();
+		if (loc!=null) {
+			dst.setPstCd(loc.getPostal());
+			dst.setCtry(loc.getCountry().getCountryCode());
+			dst.setTwnNm(loc.getCity());
+			dst.setStrtNm(loc.getAddress1());
+		}
+		return dst;
+	}
+	
+	private MLocation getRemitToLocation(MBPartner bp) {
+		
+		MBPartnerLocation[] locations = bp.getLocations(true);
+		MLocation loc = null;
+		
+		if (locations!=null && locations.length>0) {
+			if (locations.length==1) {
+				return locations[0].getLocation(false);
+			}
+			for (MBPartnerLocation bloc : locations) {
+				if (bloc.isRemitTo()) {
+					loc = bloc.getLocation(false);
+					break;
+				}
+			}
+		}
+		
+		return loc;
+		
 	}
 
 	/**
